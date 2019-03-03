95489658ee7d5a6e7160f35fb5e07aea1e58dbaf
Teesit Makapunyo <oc211088@gmail.com>
2019-03-03 15:17:50 +0700

    add resources routes; update views (criteria, criteria_group, datafile, licensee, routes, violation) (untest)
    --HG--
    branch : rocky

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :routes
   resources :licensees
+  resources :criteria
+  resources :datafiles
+  resources :attachments
+  resources :criteria_groups
+  resources :violations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
