f3ffaa2f4f86d59bc4074018d6bc34b8e91660c4
Teesit Makapunyo <oc211088@gmail.com>
2019-03-15 10:51:30 +0700

    a little tweak on ApplicationRecord.enum_to_st
    --HG--
    branch : rocky

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,9 +3,9 @@ class ApplicationRecord < ActiveRecord::Base
 
   def self.enum_to_st(enum_name,enum_value)
     if enum_value == nil
-      return '<nil>'
+      return nil
     end
-    a = "activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}";
-    return I18n.t(a);
+    a = "activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}"
+    return I18n.t(a)
   end
 end
