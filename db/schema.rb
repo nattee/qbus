787b2e63eced6af33b64c786960be720230d8f22
Nattee Niparnan <nattee@gmail.com>
2019-03-18 21:27:12 +0700

    fix process/evaluation for new evidence

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_031420) do
+ActiveRecord::Schema.define(version: 2019_03_18_135742) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -73,8 +73,10 @@ ActiveRecord::Schema.define(version: 2019_03_18_031420) do
     t.bigint "criterium_attachment_id"
     t.bigint "application_id"
     t.integer "attachment_type"
+    t.bigint "evidence_id"
     t.index ["application_id"], name: "index_attachments_on_application_id"
     t.index ["criterium_attachment_id"], name: "index_attachments_on_criterium_attachment_id"
+    t.index ["evidence_id"], name: "index_attachments_on_evidence_id"
   end
 
   create_table "cars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
