ff58bd413c787f6ae86522d89147dfd7e95fffbc
Nattee Niparnan <nattee@gmail.com>
2019-04-06 17:35:31 +0700

    - add config/secret.yml - add options for gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,22 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "www.yourhost.com" }
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => Rails.application.secrets.mail[:username],
+  :password             => Rails.application.secrets.mail[:password],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
