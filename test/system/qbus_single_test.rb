dc76c729072e45fad0364285e20855f900d48be7
Teesit Makapunyo <oc211088@gmail.com>
2019-04-17 17:13:26 +0700

    fixed log out on side bar not working; still testing ...
    --HG--
    branch : rocky

--- test/system/qbus_single_test.rb
+++ test/system/qbus_single_test.rb
@@ -8,52 +8,19 @@ class QbusTest < ApplicationSystemTestCase
     @user_licensee = User.find(3)
   end
 
-  test "test offical make visit report" do
+  test "test licensee send evidence" do
     visit root_url
     assert_selector 'form.session' # root
-    fill_in 'session[email]', with: @user_offical.email
+    fill_in 'session[email]', with: @user_licensee.email
     fill_in 'session[password]', with: 'testtest'
     click_on 'เข้าสู่ระบบ'
     assert_text 'เข้าสู่ระบบเรียบร้อย' # root (logged in)
     visit process_dashboard_url
-    assert_text 'ระบบงานนัดหมาย และ ตรวจหน้างาน'
-    click_on 'นัดหมาย'
-    assert_text 'ใบสมัครที่รอการตรวจหน้างาน'
-    click_on 'บันทึกผลตรวจหน้างาน', match: :first
-    assert_text 'การบันทึกผลการตรวจหน้างาน'
-    fill_in 'application[visited_date]', with: 1.day.since
-    fill_in 'application[visitor]', with: 'test application visitor'
-    fill_in 'application[visitor_position]', with: 'test application visitor position'
-    fill_in 'application[visitor_tel]', with: 'test application visitor tel'
-    fill_in 'application[visitor_email]', with: 'visitor@example.com'
-    fill_in 'application[visit_problem]', with: 'test visit problem'
-    fill_in 'application[visit_problem_cause]', with: 'test visit problem cause'
-    fill_in 'application[visit_tax_accrued]', with: '10'
-    fill_in 'application[visit_tax_89]', with: '100'
-    choose 'application_visit_car1_chassis_1', allow_label_click: true
-    choose 'application_visit_car2_chassis_1', allow_label_click: true
-    choose 'application_visit_car3_chassis_1', allow_label_click: true
-    choose 'application_visit_car4_chassis_1', allow_label_click: true
-    choose 'application_visit_car1_tire_1', allow_label_click: true
-    choose 'application_visit_car2_tire_1', allow_label_click: true
-    choose 'application_visit_car3_tire_1', allow_label_click: true
-    choose 'application_visit_car4_tire_1', allow_label_click: true
-    choose 'application_visit_car1_light_1', allow_label_click: true
-    choose 'application_visit_car2_light_1', allow_label_click: true
-    choose 'application_visit_car3_light_1', allow_label_click: true
-    choose 'application_visit_car4_light_1', allow_label_click: true
-    choose 'application_visit_car1_windshield_1', allow_label_click: true
-    choose 'application_visit_car2_windshield_1', allow_label_click: true
-    choose 'application_visit_car3_windshield_1', allow_label_click: true
-    choose 'application_visit_car4_windshield_1', allow_label_click: true
-    choose 'application_visit_result_ดี', allow_label_click: true
-    fill_in 'application[visit_comment]', with: 'test visit comment'
-    click_on 'บันทึก'
-    assert_text 'บันทึกผลการตรวจหน้างานเรียบร้อย'
-    click_on 'ยืนยันผลการตรวจหน้างาน'
-    alert = page.driver.browser.switch_to.alert
-    alert.accept
-    assert_text 'ยืนยันผลการตรวจหน้างานเรียบร้อย'
-    assert_text 'ใบสมัครที่ได้รับการตรวจหน้างานแล้ว'
+    assert_text 'ใบสมัครรอการดำเนินการโดยผู้ประกอบการ'
+    assert_text 'ใบสมัครรอการตรวจสอบโดยเจ้าหน้าที่'
+    assert_text 'ใบสมัครดำเนินการเสร็จแล้ว'
+    assert_text 'รอหลักฐานประกอบการประเมิน'
+    click_on 'ส่งหลักฐาน', match: :first
+    # TODO: continue after issue #52 is fixed
   end
 end
